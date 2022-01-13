727cf3f2b698370ec47982d92039d77fa018d8e0
Lucas-COX <lucas.cox@epitech.eu>
2022-01-13 16:38:55 +0100

    feat(platform): add card type

--- config/types.ts
+++ config/types.ts
@@ -4,7 +4,39 @@ export interface Deck {
     side: string[];
 }
 
-export interface card {
+export interface card_set {
+    set_name: string;
+    set_code: string;
+    set_rarity: string;
+    set_rarity_code: string;
+    set_price: string;
+}
+
+export interface card_images {
+    id: number;
+    image_url: string;
+    image_url_small: string;
+}
+
+export interface card_prices {
+    cardmarket_price: string;
+    tcgplayer_price: string;
+    ebay_price: string;
+    amazon_price: string;
+    coolstuffinc_price: string;
+}
+
+export interface card  {
     id: number;
     name: string;
+    desc: string;
+    atk?: number;
+    def?: number;
+    level?: number;
+    race?: string;
+    attribute?: string;
+    archetype?: string;
+    sets: card_set[];
+    card_images: card_images[];
+    card_prices: card_prices[];
 }
--- services/getCard.ts
+++ services/getCard.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import type { card } from "../config/types"
 
-export function getCardById(id: string): Promise<any | null> {
+export function getCardById(id: string): Promise<card | null> {
     return new Promise ((res, rej) => {
         axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${id}`).then((result) => {
-            res(result.data.data[0]);
+            res(result.data.data[0] as card);
         }).catch((err) => { rej(err)} )
     })
 }
